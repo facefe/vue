db639863ea69d429958f7af9c5effa7cff2be2c5
memory <2410732744@qq.com>
2019-02-06 22:15:40 +0800

    fix err

--- css/promies/promise_mine.js
+++ css/promies/promise_mine.js
@@ -10,7 +10,7 @@ function Promise_mine(func) {
         self.state = 'fulfilled'
         self.asyncdata = data
         self.resolveFuncList.forEach(function (asyncFunc) {
-            asyncFunc()
+            asyncFunc(data)
 
         })
     }
@@ -19,7 +19,7 @@ function Promise_mine(func) {
         self.state = 'reject'
         self.asyncdata = data
         self.rejectFuncList.forEach(function (asyncFunc) {
-            asyncFunc()
+            asyncFunc(data)
 
         })
     }
@@ -30,26 +30,23 @@ function Promise_mine(func) {
     }
 }
 
-/**
- * 没有考虑kong then 的情况，
- */
 Promise_mine.prototype.then = function (func_resolve, func_reject) {
     let self = this
+
     // 还有链式执行。
-    let newP = new Promise_mine(function (res, rej) {
+    let newPro = new Promise_mine(function (res, rej) {
         if (self.state === 'fulfilled') {
             try {
-                let newProVal = func_resolve(self.asyncdata)
-                // 如果是同步函数,返回值会直接直接返回给 res 
-                res(newProVal)
-            } catch (d) {
+                let newRes = func_resolve(self.asyncdata)
+                res(newRes)
+            } catch (e) {
                 rej(e)
             }
         }
         if (self.state === 'reject') {
             try {
-                let newProVal = func_reject(self.asyncdata)
-                res(newProVal)
+                let newRes = func_reject(self.asyncdata)
+                res(newRes)
             } catch (e) {
                 rej(e)
             }
@@ -61,19 +58,16 @@ Promise_mine.prototype.then = function (func_resolve, func_reject) {
             if (func_reject !== undefined) {
                 self.resolveFuncList.push(function () {
                     try {
-                        let newProVal = func_reject(self.asyncdata)
-                        res(newProVal)
+                        let newRes = func_resolve(self.asyncdata)
+                        res(newRes)
                     } catch (e) {
                         rej(e)
                     }
-                    // 但是这样的话, 如果then里面注册的是异步函数的话，就会没有办法执行。
                 })
                 self.rejectFuncList.push(function () {
                     try {
-                        let newProVal = func_reject(self.asyncdata)
-                        // 如果 then 里面传递的是异步函数,返回值也会传递, 因为这部分是包围在一个 function 里面 
-                        // 而且 这个这个 function 是直接传递给 promise 的
-                        res(newProVal)
+                        let newRes = func_reject(self.asyncdata)
+                        res(newRes)
                     } catch (e) {
                         rej(e)
                     }
@@ -84,8 +78,8 @@ Promise_mine.prototype.then = function (func_resolve, func_reject) {
                 } else {
                     self.resolveFuncList.push(function () {
                         try {
-                            let newProVal = func_reject(self.asyncdata)
-                            res(newProVal)
+                            let newRes = func_resolve(self.asyncdata)
+                            res(newRes)
                         } catch (e) {
                             rej(e)
                         }
@@ -96,7 +90,6 @@ Promise_mine.prototype.then = function (func_resolve, func_reject) {
         }
     })
 
-    return newP
-
+    return newPro
 
 }
